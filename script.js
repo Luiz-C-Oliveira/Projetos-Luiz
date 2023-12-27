d32fee6fbd12a22264a5f1a3291019ef320e18c4
Luiz-C-Oliveira <146564270+Luiz-C-Oliveira@users.noreply.github.com>
2023-12-26 21:24:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,113 @@
-let count = 1;
-document.getElementById('radio1').checked = true
+// Select button
+var button = document.getElementById('read_button');
 
-setInterval( function(){
-    nextImage();
-}, 5000)
+// Click Event
+button.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('.card');
 
-function nextImage(){
-    count++;
-    if(count>4){
-        count = 1;
+    // Add/Remove Class Active
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+        // Change button text if has class active
+        return button.textContent = 'Ler Menos';
     }
+    
+    // Change button text if hasn't class active
+    button.textContent = 'Ler Mais';
+});
 
-    document.getElementById("radio"+count).checked = true;
-}
+// Select button
+var button = document.getElementById('read_button1');
+
+// Click Event
+button.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('.card1');
+
+    // Add/Remove Class Active
+    card.classList.toggle('active1');
+
+    if (card.classList.contains('active1')) {
+        // Change button text if has class active
+        return button.textContent = 'Ler Menos';
+    }
+    
+    // Change button text if hasn't class active
+    button.textContent = 'Ler Mais';
+});
+
+// Select button
+var button = document.getElementById('read_button2');
+
+// Click Event
+button.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('.card2');
+
+    // Add/Remove Class Active
+    card.classList.toggle('active2');
+
+    if (card.classList.contains('active2')) {
+        // Change button text if has class active
+        return button.textContent = 'Ler Menos';
+    }
+    
+    // Change button text if hasn't class active
+    button.textContent = 'Ler Mais';
+});
+
+// Select button
+var button = document.getElementById('read_button3');
+
+// Click Event
+button.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('.card3');
+
+    // Add/Remove Class Active
+    card.classList.toggle('active3');
+
+    if (card.classList.contains('active3')) {
+        // Change button text if has class active
+        return button.textContent = 'Ler Menos';
+    }
+    
+    // Change button text if hasn't class active
+    button.textContent = 'Ler Mais';
+});
+
+// Select button
+var button = document.getElementById('read_button4');
+
+// Click Event
+button.addEventListener('click', function() {
+    // Select card
+    var card = document.querySelector('.card4');
+
+    // Add/Remove Class Active
+    card.classList.toggle('active4');
+
+    if (card.classList.contains('active4')) {
+        // Change button text if has class active
+        return button.textContent = 'Ler Menos';
+    }
+    
+    // Change button text if hasn't class active
+    button.textContent = 'Ler Mais';
+});
+
+var stars = document.querySelectorAll('.star-icon');
+                  
+document.addEventListener('click', function(e){
+  var classStar = e.target.classList;
+  if(!classStar.contains('ativo')){
+    stars.forEach(function(star){
+      star.classList.remove('ativo');
+    });
+    classStar.add('ativo');
+    console.log(e.target.getAttribute('data-avaliacao'));
+  }
+});
+
